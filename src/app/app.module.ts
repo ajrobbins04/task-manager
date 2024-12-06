2604b690d336ed90bff1769ee8293d582e39a97b
ajrobbins44 <rob16003@byui.edu>
2024-12-05 21:17:55 -0800

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+import { TaskEditComponent } from './tasks/task-edit/task-edit.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { TaskItemComponent } from './tasks/task-list/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TasksComponent,
+    TaskDetailComponent,
+    TaskEditComponent,
+    TaskListComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
