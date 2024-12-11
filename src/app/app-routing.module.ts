72de25cb9de177252b820451e223dd64af3753f5
ajrobbins44 <rob16003@byui.edu>
2024-12-10 22:06:23 -0800

    routing module works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { TaskEditComponent } from './tasks/task-edit/task-edit.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/tasks', pathMatch: 'full' },
-    { path: 'documents', component: TasksComponent,
+    { path: 'tasks', component: TasksComponent,
         children: [
           { path: 'new', component: TaskEditComponent },
           { path: ':id/edit', component: TaskEditComponent },
